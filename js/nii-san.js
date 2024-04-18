3d1bdbcb4a1a389ec5a93e0359a23294301dd9ab
rafly <raflyadityaray3@gmail.com>
2024-04-18 20:10:55 +0700

    clone-web

---
+++ js/nii-san.js
@@ -0,0 +1,7 @@
+const navigation = document.querySelector('.navbar-nav');
+// const input = document.querySelector('input');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navigation.classList.toggle('active')
+    // input.classList.toggle('active')
+};
